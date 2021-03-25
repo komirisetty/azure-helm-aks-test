597d1280f278a16fa7e062a63be4b39febb7b340
komirisetty <knagesh35@gmail.com>
2021-03-25 13:05:45 +0530

    Update rename-batchscript.bat

--- rename-batchscript.bat
+++ rename-batchscript.bat
@@ -3,6 +3,12 @@ setlocal enableDelayedExpansion
 for /D %%s in (.\*) do (
   for %%F in (%%s\*.txt) do (
     set "name=%%F"
-    ren "!name!" "newname"
+    FOR /F "tokens=1,2 delims==" %%x in (vv.txt) DO (
+      set "on=%%x"
+      set "nn=%%y"
+      if "!name!" == "!on!" (
+        ren "!name!" "!nn!"
+      )
+    )
   )
 )
